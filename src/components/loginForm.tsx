d2107176e899127c6b898045f9704e4d7c992d8e
chethanmv-31 <chethan.makodu@gmail.com>
2024-08-19 22:50:00 +0530

    redux and form hook setup done

--- src/components/loginForm.tsx
+++ src/components/loginForm.tsx
@@ -1,4 +1,7 @@
 "use client";
+
+import { updateForm } from "@/store/actions/formActions";
+import { RootState } from "@/store/store";
 import styled from "@emotion/styled";
 import { VisibilityOff, Visibility } from "@mui/icons-material";
 import {
@@ -11,15 +14,43 @@ import {
 } from "@mui/material";
 import Link from "next/link";
 import React, { useState } from "react";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import { useDispatch, useSelector } from "react-redux";
 
 interface TypeProps {
   header: string;
   subHeader: string;
-  isSignUp: boolean; // Optional prop
+  isSignUp: boolean;
 }
 
+interface IFormInput {
+  firstName: string;
+  lastName: string;
+  userName: string;
+  email: string;
+  password: string;
+}
+
+const CustomTextField = styled(TextField)({
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      borderColor: "lightgray",
+    },
+    "&:hover fieldset": {
+      borderColor: "lightgray",
+    },
+    "&.Mui-focused fieldset": {
+      borderColor: "lightgray",
+      outline: "none",
+    },
+  },
+});
+
 const LoginForm = ({ isSignUp, header, subHeader }: TypeProps) => {
-  const [showPassword, setShowPassword] = useState<Boolean>(false);
+  const { control, handleSubmit } = useForm<IFormInput>();
+  const dispatch = useDispatch();
+  const formState = useSelector((state: RootState) => state.form);
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
 
@@ -28,20 +59,12 @@ const LoginForm = ({ isSignUp, header, subHeader }: TypeProps) => {
   ) => {
     event.preventDefault();
   };
-  const CustomTextField = styled(TextField)({
-    "& .MuiOutlinedInput-root": {
-      "& fieldset": {
-        borderColor: "lightgray",
-      },
-      "&:hover fieldset": {
-        borderColor: "lightgray",
-      },
-      "&.Mui-focused fieldset": {
-        borderColor: "lightgray",
-        outline: "none",
-      },
-    },
-  });
+
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    dispatch(updateForm(data));
+    console.log("data====", data);
+  };
+
   return (
     <div
       className={`bg-white w-[480px] ${
@@ -51,8 +74,9 @@ const LoginForm = ({ isSignUp, header, subHeader }: TypeProps) => {
       <div>
         <img src="/assets/Logo 1.png" className="m-auto w-36 pt-5 mb-10" />
 
-        <p className="text-[20px] mb-2"> {header}</p>
+        <p className="text-[20px] mb-2">{header}</p>
         <p className="text-[15px] text-[#ABABAB] mb-7">{subHeader}</p>
+
         <Box
           component="form"
           sx={{
@@ -61,63 +85,118 @@ const LoginForm = ({ isSignUp, header, subHeader }: TypeProps) => {
           noValidate
           autoComplete="off"
           className="mb-12"
+          onSubmit={handleSubmit(onSubmit)}
         >
           <div className="mb-5">
             <p className="text-left text-[16px] font-semibold">Username</p>
-            <CustomTextField id="outlined-size-small" size="small" />
-          </div>
-          {isSignUp && (
-            <div className="mb-5">
-              <p className="text-left text-[16px] font-semibold">Email</p>
-              <CustomTextField id="outlined-size-small" size="small" />
-            </div>
-          )}
-          {isSignUp && (
-            <div className="mb-5">
-              <p className="text-left text-[16px] font-semibold">First name</p>
-              <CustomTextField id="outlined-size-small" size="small" />
-            </div>
-          )}
-          {isSignUp && (
-            <div className="mb-5">
-              <p className="text-left text-[16px] font-semibold">Last name</p>
-              <CustomTextField id="outlined-size-small" size="small" />
-            </div>
-          )}
-
-          <div className=" mb-5">
-            <p className="text-left text-[16px] font-semibold">Password</p>
-            <OutlinedInput
-              id="outlined-adornment-password"
-              type={showPassword ? "text" : "password"}
-              sx={{
-                "& .MuiOutlinedInput-notchedOutline": {
-                  borderColor: "lightgray",
-                },
-                "&:hover .MuiOutlinedInput-notchedOutline": {
-                  borderColor: "lightgray",
-                },
-                "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
-                  borderColor: "lightgray",
-                  borderWidth: 1,
-                },
-              }}
-              size="small"
-              fullWidth
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleMouseDownPassword}
-                    edge="end"
-                  >
-                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              }
+            <Controller
+              name="userName"
+              control={control}
+              render={({ field }) => (
+                <CustomTextField
+                  {...field}
+                  size="small"
+                  autoComplete="username"
+                />
+              )}
             />
           </div>
+
+          {isSignUp && (
+            <>
+              <div className="mb-5">
+                <p className="text-left text-[16px] font-semibold">Email</p>
+                <Controller
+                  name="email"
+                  control={control}
+                  render={({ field }) => (
+                    <CustomTextField
+                      {...field}
+                      size="small"
+                      autoComplete="email"
+                    />
+                  )}
+                />
+              </div>
+              <div className="mb-5">
+                <p className="text-left text-[16px] font-semibold">First name</p>
+                <Controller
+                  name="firstName"
+                  control={control}
+                  render={({ field }) => (
+                    <CustomTextField
+                      {...field}
+                      size="small"
+                      autoComplete="given-name"
+                    />
+                  )}
+                />
+              </div>
+              <div className="mb-5">
+                <p className="text-left text-[16px] font-semibold">Last name</p>
+                <Controller
+                  name="lastName"
+                  control={control}
+                  render={({ field }) => (
+                    <CustomTextField
+                      {...field}
+                      size="small"
+                      autoComplete="family-name"
+                    />
+                  )}
+                />
+              </div>
+            </>
+          )}
+
+          <div className="mb-5">
+            <p className="text-left text-[16px] font-semibold">Password</p>
+            <Controller
+              name="password"
+              control={control}
+              render={({ field }) => (
+                <OutlinedInput
+                  {...field}
+                  id="outlined-adornment-password"
+                  type={showPassword ? "text" : "password"}
+                  sx={{
+                    "& .MuiOutlinedInput-notchedOutline": {
+                      borderColor: "lightgray",
+                    },
+                    "&:hover .MuiOutlinedInput-notchedOutline": {
+                      borderColor: "lightgray",
+                    },
+                    "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                      borderColor: "lightgray",
+                      borderWidth: 1,
+                    },
+                  }}
+                  size="small"
+                  fullWidth
+                  autoComplete="current-password"
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                />
+              )}
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="bg-[#DF7D3A] text-white w-[100%] pt-3 pb-3 rounded-md mb-12 hover:bg-[#df7c3ae4]"
+          >
+            {isSignUp ? "Register" : "Login"}
+          </button>
         </Box>
       </div>
 
@@ -137,21 +216,13 @@ const LoginForm = ({ isSignUp, header, subHeader }: TypeProps) => {
           <p className="border-b-2 border-gray-600">Forgot password?</p>
         </div>
       )}
-      {isSignUp ? (
-        <button className="bg-[#DF7D3A] text-white w-[100%] pt-2 pb-2 rounded-md mb-12">
-          Register
-        </button>
-      ) : (
-        <button className="bg-[#DF7D3A] text-white w-[100%] pt-2 pb-2 rounded-md mb-12">
-          Login
-        </button>
-      )}
+
       <div className="flex justify-between items-center">
         <div>
           {isSignUp ? (
             <>
               <span>Already a User?</span>
-              <Link href={""}>
+              <Link href="/login">
                 <span className="text-[16px] border-b-2 border-gray-600 ml-1">
                   Login now
                 </span>
@@ -160,7 +231,7 @@ const LoginForm = ({ isSignUp, header, subHeader }: TypeProps) => {
           ) : (
             <>
               <span>New User?</span>
-              <Link href={""}>
+              <Link href="/register">
                 <span className="text-[16px] border-b-2 border-gray-600 ml-1">
                   Register Here
                 </span>
@@ -168,7 +239,7 @@ const LoginForm = ({ isSignUp, header, subHeader }: TypeProps) => {
             </>
           )}
         </div>
-        <p>Use as Guest </p>
+        <p>Use as Guest</p>
       </div>
     </div>
   );
