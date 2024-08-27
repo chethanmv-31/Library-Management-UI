5bf3735a15a7ef2757783a6414180dd196968acf
chethanmv-31 <chethan.makodu@gmail.com>
2024-08-27 11:54:18 +0530

    created home page

--- src/components/home/BookCard.tsx
+++ src/components/home/BookCard.tsx
@@ -1,17 +1,37 @@
 import React from "react";
 import Image from "next/image";
 
-const BookCard = () => {
+interface types {
+  isIntro: boolean;
+}
+const BookCard = ({ isIntro }: types) => {
+  const width = isIntro ? 123 : 133
   return (
-    <div className="shadow-lg rounded-lg h-[203px] w-[167px] flex items-center justify-center border drop-shadow-lg">
-      <Image
-        src="/assets/Rectangle 12.png"
-        alt={""}
-        width={123}
-        height={173}
-        className="rounded-md"
-      />
-    </div>
+    <div
+      className={`${
+        isIntro
+          ? "shadow-lg border drop-shadow-lg w-[167px]  h-[203px] "
+          : "w-[165px]  flex-col h-[275px] py-[17px]"
+      } rounded-lg flex items-center justify-center `}
+    >
+     
+        <Image
+          src="/assets/Rectangle 12.png"
+          alt={""}
+          width={width}
+          height={173}
+          className="rounded-md"
+        />
+        {!isIntro && (
+          <div >
+            <p className="text-[13px] pt-2">The Design of Every..</p>
+            <p className="text-[11px] py-[3px]">
+              Don Norman,<span> 1988</span>
+            </p>
+            <p  className="text-[11px]">4.5/ <span className="text-gray-400">5</span></p>
+          </div>
+        )}
+      </div>
   );
 };
 
--- src/components/home/NewArrivals.tsx
+++ src/components/home/NewArrivals.tsx
@@ -3,15 +3,15 @@ import BookCard from "./BookCard";
 
 const NewArrivals = () => {
   return (
-    <div className="bg-gradient-to-b from-[#EB5231] to-[#8A317C] rounded-lg p-[2px] text-white h-[233px] mx-auto shadow-lg relative flex-grow w-[861px]">
+    <div className="bg-gradient-to-b from-[#EB5231] to-[#8A317C] rounded-lg p-[2px] text-white h-[241px] mx-auto shadow-lg relative flex-grow w-[861px]">
       <p className="text-[25px] font-[500] transform rotate-[-90deg] absolute left-[18px] top-[90%] translate-y-[-50%] origin-top-left">
         New Arrivals
       </p>
 
-      <div className="bg-white flex-1 ml-[60px] h-[229px] w-[857px] rounded-tr-lg rounded-br-lg flex items-center overflow-x-scroll overflow-y-hidden scrollbar-hide">
+      <div className="bg-white flex-1 ml-[60px] h-[237px] w-[887px] rounded-tr-lg rounded-br-lg flex items-center overflow-x-scroll overflow-y-hidden scrollbar-hide">
         {[0, 1, 2, 3, 4, 5, 6, 7].map((item, index) => (
           <div key={item} className="flex-shrink-0 w-[167px] mx-7">
-            <BookCard />
+            <BookCard isIntro={true} />
           </div>
         ))}
       </div>
